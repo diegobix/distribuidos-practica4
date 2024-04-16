1dca40892644b306e87fd51fd608ed3ec1180e2e
Diego Arenas <diegobix97@gmail.com>
2024-04-16 13:39:00 +0200

    close all button

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 const path = require('path')
 
+var enabled = true;
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  if (enabled) res.sendFile(path.join(__dirname, 'index.html'));
+  else res.sendFile(path.join(__dirname, 'closed.html'));
 });
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
+
+  socket.on('close message', () => {
+    enabled = false;
+    io.emit('close message');
+  })
 });
 
 http.listen(port, host, () => {
